791ed9a583d17d22fb59517b645a3f8c7f043b49
sutharsan sparkz <62829664+Sparkz-technology@users.noreply.github.com>
2020-04-20 20:37:15 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#selecting arch
+arch=$(dpkg --print-architecture)
+#selecting commands
+update="apt update -y >/dev/null"
+upgrade="apt upgrade -y >/dev/null"
+iwget="apt install wget zip unzip figlet -y"
+#simple condition sentence
+
+sleep 5
+if [ $arch == 'aarch64' ]
+then 
+echo -e "\033[92m[•] \033[93mInstalling the requirements... "
+#require
+echo -e "\033[92m"
+toilet -f mono12 ngrok
+echo -e "\033[94m"
+toilet -f mono12 For
+echo -e "\033[95m"
+toilet $arch
+wget "https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip"
+unzip "ngrok-2.2.8-linux-arm64.zip"
+chmod 777 ngrok
+else 
+echo -e "\033[92m[•] \033[93mInstalling the requirements... "
+#require
+echo -e "\033[92m"
+figlet "ngrok"
+echo -e "\033[94m"
+figlet "for"
+echo -e "\033[95m"
+figlet $arch
+wget https://github.com/Ahmedmahmed8a/ngrok-arm/raw/master/ngrok-arm
+mv ngrok-arm ngrok
+chmod 777 ngrok
+fi
+echo -e """\033[92m 
+ngrok has been installed successfully 
+"""
