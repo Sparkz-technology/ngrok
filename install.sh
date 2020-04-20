72fb6d69f8ecd71cd26e9df8c901025d39f5ef85
sutharsan sparkz <62829664+Sparkz-technology@users.noreply.github.com>
2020-04-20 21:02:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,20 +3,20 @@
 #selecting arch
 arch=$(dpkg --print-architecture)
 #selecting commands
-update="apt update -y >/dev/null"
-upgrade="apt upgrade -y >/dev/null"
-iwget="apt install wget zip unzip figlet -y"
+#update="apt update -y >/dev/null"
+#upgrade="apt upgrade -y >/dev/null"
+#iwget="apt install wget zip unzip figlet -y"
 #simple condition sentence
-require(){
-	eval $update
-	eval $upgrade
-	eval $iwget
+#require(){
+#	eval $update
+#	eval $upgrade
+#	eval $iwget
 }
 sleep 5
 if [ $arch == 'aarch64' ]
 then 
 echo -e "\033[92m[•] \033[93mInstalling the requirements... "
-require
+#require
 echo -e "\033[92m"
 toilet -f mono12 ngrok
 wget "https://bin.equinox.io/a/nmkK3DkqZEB/ngrok-2.2.8-linux-arm64.zip"
@@ -24,7 +24,7 @@ unzip "ngrok-2.2.8-linux-arm64.zip"
 chmod 777 ngrok
 else 
 echo -e "\033[92m[•] \033[93mInstalling the requirements... "
-require
+#require
 echo -e "\033[92m"
 toilet -f mono12 ngrok
 wget https://github.com/sparkz-technology/ngrok-arm/raw/master/ngrok-arm
