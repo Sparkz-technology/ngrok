0282d97fc499f0c373a81b808a462c2521e6d473
sutharsan sparkz <62829664+Sparkz-technology@users.noreply.github.com>
2020-04-20 21:44:09 +0530

    Add files via upload

---
+++ sparkz.sh
@@ -0,0 +1,12 @@
+#!/bin/base
+
+clear
+echo -e "\e[1;32m"
+toilet -f mono12  NGROK | lolcat
+echo -e "\e[1;34m Created By @sparkz_technology \e[1;31m              v6.sparkz"
+
+wget https://github.com/sparkz-technology/ngrok-arm/raw/master/ngrok
+chmod 777 ngrok
+echo -e """\033[92m 
+ngrok has been installed successfully 
+"""
